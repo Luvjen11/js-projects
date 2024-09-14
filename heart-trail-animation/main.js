89e0d4ed38e226f67325283f4257b6c3995bfa1f
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-09-14 18:28:20 +0100

    change heart colors

--- heart-trail-animation/main.js
+++ heart-trail-animation/main.js
@@ -6,6 +6,11 @@ heart.addEventListener("mousemove", (event)=>{
     const spanEl = document.createElement("span"); //creates a span each time mouse moves
     spanEl.style.left = xPos + "px";
     spanEl.style.top = yPos + "px";
-
+    const size = Math.random()*100; 
+    spanEl.style.width = size + "px"; /* to change the size of hearts*/
+    spanEl.style.height = size + "px"; /* to change the size of hearts*/
     heart.appendChild(spanEl);
+    setTimeout(() => {
+        spanEl.remove();
+    }, 3000);
 })
