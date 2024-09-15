eb182db5e435b4f909b51ea97127007a79fe3b34
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-09-15 21:51:54 +0100

    complete display of affirmations

--- daily-affirmations/main.js
+++ daily-affirmations/main.js
@@ -1,7 +1,24 @@
-const container = document.querySelector(".container");
+const containerEl = document.querySelector(".container");
 
-const affirmations = ["capable of greatness", "in control of my destiny", " enough, just as I am", "surrounded by abundance", "free from fear and doubt", "worthy of love and respect"  ] ;
+const affirmations = ["capable of greatness", "in control of my destiny", " enough, just as I am", "surrounded by abundance", "free from fear and doubt", "worthy of love and respect"];
 
-container.innerHTML = `
-<h1>I am capable of greatness</h1>
-`;
+let affirmationsIndex = 0;
+let character = 0;
+
+display();
+
+function display() {
+    character++;
+    containerEl.innerHTML = `
+    <h1>I am ${affirmations[affirmationsIndex].slice(0, character)}</h1>
+    `;
+    if (character === affirmations[affirmationsIndex].length) {
+        affirmationsIndex++
+        character = 0;
+    }
+
+    if (affirmationsIndex === affirmations.length) {
+        affirmationsIndex = 0;
+    }
+    setTimeout(display, 175)
+}
