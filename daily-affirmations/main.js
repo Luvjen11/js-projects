f97688a28065d0e7c4c41a381374dfca9d8683fc
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-09-15 18:21:33 +0100

    create js file and array of affirmations

---
+++ daily-affirmations/main.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+
+const affirmations = ["capable of greatness", "in control of my destiny", " enough, just as I am", "surrounded by abundance", "free from fear and doubt", "worthy of love and respect"  ] ;
+
+container.innerHTML = `
+<h1>I am capable of greatness</h1>
+`;
