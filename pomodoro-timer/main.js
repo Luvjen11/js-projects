247b60d40b314f3724354f6b083e4c6175c44e3b
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-09-16 19:55:22 +0100

    create 25 minutes pomodoro timer

---
+++ pomodoro-timer/main.js
@@ -0,0 +1,44 @@
+const startBtn = document.getElementById("Start");
+const stopBtn = document.getElementById("Stop");
+const resetBtn = document.getElementById("Reset");
+const timer = document.getElementById("timer");
+
+
+let interval;
+let timeRemaining= 1500;
+
+function updateTimer() {
+    let minutes = Math.floor(timeRemaining/ 60);
+    let seconds = timeRemaining % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+
+    timer.innerHTML = formattedTime;
+}
+
+function startTimer(){
+    interval = setInterval(() => {
+        timeRemaining--;
+        updateTimer();
+        if (timeRemaining === 0){
+            clearInterval(interval)
+            alert("Time's up!");
+            timeRemaining = 1500;
+            updateTimer();
+        }
+    }, 1000);
+}
+
+function stopTimer(){
+    clearInterval(interval);
+}
+
+function resetTimer(){
+    clearInterval(interval);
+    timeRemaining = 1500;
+    updateTimer();
+}
+
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
