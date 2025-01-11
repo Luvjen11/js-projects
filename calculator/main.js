e437bc4d90acffae971339e5d33bf2e0bb548f80
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-01-11 20:26:14 +0000

    add basic math functions (add, subtract, multiply, divide)

---
+++ calculator/main.js
@@ -0,0 +1,29 @@
+// Adds two numbers and returns the result
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+// subtracts two numbers and returns the result
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+// multiplies two numbers and returns the result
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+// divides two numbers and returns the result
+function divide(number1, number2) {
+    if (number2 === 0) {
+        return "Error: You can't divide by zero dummie!";
+    }
+    return number1 / number2;
+}
+
+// testing if functions are working
+console.log(add(3, 5)); 
+console.log(subtract(10, 4)); 
+console.log(multiply(2, 3)); 
+console.log(divide(8, 2)); 
+console.log(divide(5, 0));
